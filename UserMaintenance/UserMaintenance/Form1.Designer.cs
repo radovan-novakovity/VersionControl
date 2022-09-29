3689a7eba3004fcb86bc964a05fe5054f67fba9d
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-09-29 18:41:41 +0200

    Added the function to add Last Name and First name, which in turn ends up being displayed in the Listbox

--- UserMaintenance/UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/UserMaintenance/Form1.Designer.cs
@@ -49,29 +49,29 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(245, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(58, 13);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "Last Name";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(245, 67);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(57, 13);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "First Name";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(287, 30);
+            this.textBox1.Location = new System.Drawing.Point(309, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(159, 20);
             this.textBox1.TabIndex = 3;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(286, 64);
+            this.textBox2.Location = new System.Drawing.Point(309, 64);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(159, 20);
             this.textBox2.TabIndex = 4;
@@ -82,8 +82,9 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(197, 37);
             this.button1.TabIndex = 5;
-            this.button1.Text = "button1";
+            this.button1.Text = "Hozzáadás";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
