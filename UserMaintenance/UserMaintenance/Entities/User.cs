966cd5219872d873bbaa1e2437d758348439540d
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-09-29 18:07:12 +0200

    Making the Form1 design, adding a User class, and making a Resource files to store static text

---
+++ UserMaintenance/UserMaintenance/Entities/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class User
+{
+    public Guid ID { get; set; } = Guid.NewGuid();
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    // public string FullName => string.Format("{0} {1}", LastName, FirstName);
+
+    public string Fullname
+    {
+        get
+        {
+            return string.Format(
+                "{0} {1}",
+                LastName,
+                FirstName);
+        }
+    }
+}
