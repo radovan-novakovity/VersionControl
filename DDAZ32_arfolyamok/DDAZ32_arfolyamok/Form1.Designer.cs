dff768449de804172d18cbfab2c3de8ca2d8a228
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-10-21 12:15:15 +0200

    Trying for a third time.

--- DDAZ32_arfolyamok/DDAZ32_arfolyamok/Form1.Designer.cs
+++ DDAZ32_arfolyamok/DDAZ32_arfolyamok/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace DDAZ32_arfolyamok
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Form1gg";
+            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
