1b75d34361c45dce069adc887e2d190b40e78a52
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-11-04 13:09:34 +0100

    Got to the CarFactory implementation part of the task.

---
+++ DDAZ32_gyar/DDAZ32_gyar/Enteties/BallFactory.cs
@@ -0,0 +1,17 @@
+﻿using DDAZ32_gyar.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDAZ32_gyar.Enteties
+{
+    public class BallFactory : IToyFactory
+    {
+        public Toy CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
