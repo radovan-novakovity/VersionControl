9a3aa226b542c0a27bf674cbc9b7b25db4e6214e
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-11-11 13:08:07 +0100

    Almost completed part 9

--- DDAZ32_value/DDAZ32_value/Form1.cs
+++ DDAZ32_value/DDAZ32_value/Form1.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,12 +18,34 @@ namespace DDAZ32_value
         List<Tick> Ticks;
 
         List<PortfolioItem> Portfolio = new List<PortfolioItem>();
+        //List<decimal> Nyereségek = new List<decimal>();
+        List<decimal> NyereségekRendezve;
         public Form1()
         {
             InitializeComponent();
             Ticks = context.Ticks.ToList();
             dataGridView1.DataSource = Ticks;
             CreatePortfolio();
+
+            List<decimal> Nyereségek = new List<decimal>();
+            int intervalum = 30;
+            DateTime kezdőDátum = (from x in Ticks select x.TradingDay).Min();
+            DateTime záróDátum = new DateTime(2016, 12, 30);
+            TimeSpan z = záróDátum - kezdőDátum;
+            for (int i = 0; i < z.Days - intervalum; i++)
+            {
+                decimal ny = GetPortfolioValue(kezdőDátum.AddDays(i + intervalum))
+                           - GetPortfolioValue(kezdőDátum.AddDays(i));
+                Nyereségek.Add(ny);
+                Console.WriteLine(i + " " + ny);
+            }
+
+            NyereségekRendezve = (from x in Nyereségek
+                                      orderby x
+                                      select x)
+                                        .ToList();
+
+            MessageBox.Show(NyereségekRendezve[NyereségekRendezve.Count() / 5].ToString());
         }
 
         private void CreatePortfolio()
@@ -52,6 +75,32 @@ namespace DDAZ32_value
             }
 
             return value;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Vesszövel tagolt szöveg (*.csv) |*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                using (StreamWriter sw = new StreamWriter(sfd.FileName, true, Encoding.UTF8))
+                {
+                    //sw.Write("Időszak");
+                    //sw.Write("Nyereség");
+
+                    int counter = 1;
+                    foreach (var ny in NyereségekRendezve)
+                    {
+                        sw.WriteLine($"{counter};{ny.ToString()}");
+                        counter += 1;
+                    }
+                }
+            }
         }
     }
 }
