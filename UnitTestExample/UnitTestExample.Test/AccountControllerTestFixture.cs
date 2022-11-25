56bfab5e0100048d8d6b087f4c204362edb66b81
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-11-25 12:46:04 +0100

    Password validation test done

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -17,6 +17,8 @@ namespace UnitTestExample.Test
             TestCase("irf.uni-corvinus.hu", false),
             TestCase("irf@uni-corvinus.hu", true)
         ]
+
+       
         public void TestValidateEmail(string email, bool expectedResult)
         {
             
@@ -26,6 +28,24 @@ namespace UnitTestExample.Test
             var actualResult = accountController.ValidateEmail(email);
 
             
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [
+            Test,
+            TestCase("ABCD1234", false),
+            TestCase("Ab1234", false),
+            TestCase("Abcd1234", true),
+            TestCase("abcd1234", false),
+            TestCase("abcdABCD1234", false)
+        ]
+
+        public void TestValidatePassword(string password, bool expectedResult)
+        {
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidatePassword(password);
+
             Assert.AreEqual(expectedResult, actualResult);
         }
     }
