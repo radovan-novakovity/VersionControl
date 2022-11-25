e21928c3b2dac5f9521980f0aa5cf112988664b7
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-11-25 13:02:43 +0100

    Started the password validation function for the test cases

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using UnitTestExample.Controllers;
 
@@ -44,9 +45,18 @@ namespace UnitTestExample.Test
         {
             var accountController = new AccountController();
 
-            var actualResult = accountController.ValidatePassword(password);
+            //var actualResult = accountController.ValidatePassword(password);
+            var hasNumber = new Regex(@"[0-9]+");
+            var hasUpperChar = new Regex(@"[A-Z]+");
+            var hasMinimum8Chars = new Regex(@".{8,}");
+
+            var actualResult = hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum8Chars.IsMatch(password);
 
             Assert.AreEqual(expectedResult, actualResult);
+
+            
+
+            
         }
     }
 }
