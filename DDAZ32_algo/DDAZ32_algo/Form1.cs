564568fa185e88dafd022456eae2ba90bcf7a0bd
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-12-09 12:23:48 +0100

    Completed part 4

--- DDAZ32_algo/DDAZ32_algo/Form1.cs
+++ DDAZ32_algo/DDAZ32_algo/Form1.cs
@@ -29,11 +29,29 @@ namespace DDAZ32_algo
             //gc.AddPlayer();
             //gc.Start(true);
 
+            gc.GameOver += Gc_GameOver;
+
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
             gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+
+            label1.BringToFront();
+
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
         }
     }
 }
