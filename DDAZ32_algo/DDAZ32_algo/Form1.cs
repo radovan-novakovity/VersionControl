5a23a1548334197175d8d59d05a59bb36a845150
radovan-novakovity <radovan.novakovity@stud.uni-corvinus.hu>
2022-12-09 12:13:07 +0100

    Completed part 3

--- DDAZ32_algo/DDAZ32_algo/Form1.cs
+++ DDAZ32_algo/DDAZ32_algo/Form1.cs
@@ -15,14 +15,25 @@ namespace DDAZ32_algo
     {
         GameController gc = new GameController();
         GameArea ga;
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
         public Form1()
         {
             InitializeComponent();
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-            gc.AddPlayer();
-            gc.Start(true);
+            //gc.AddPlayer();
+            //gc.Start(true);
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+            gc.Start();
         }
     }
 }
